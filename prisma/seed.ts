fff85886ff69a2459dd003044c6914aaa2877a54
DiegoJohnsonL <diegojohnson46@gmail.com>
2023-08-29 11:17:16 -0500

    modified seed file

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,11 +4,16 @@ import { roles } from './roles';
 const prisma = new PrismaClient();
 
 async function main() {
-	//TODO: Create a manager role and add a single user with that role so it can login and create admin accounts
 	for (const role of roles) {
-		await prisma.role.create({
-			data: role,
+		const existingRole = await prisma.role.findFirst({
+			where: { name: role.name },
 		});
+
+		if (!existingRole) {
+			await prisma.role.create({
+				data: role,
+			});
+		}
 	}
 }
 
